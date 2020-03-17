54824e6b412682bed37fff74f603493537621e56
Po Jung Wu <wupojung@gmail.com>
2020-03-18 03:45:57 +0800

    模擬Server

---
+++ Server/Controllers/TokenController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly JwtHelpers jwt;
+        private IHttpContextAccessor _httpContextAccessor;
+
+        public TokenController(JwtHelpers jwt, IHttpContextAccessor httpContextAccessor)
+        {
+            this.jwt = jwt;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        [HttpGet, Route("")]
+        public IActionResult Index()
+        {
+            Random rnd = new Random();
+
+            return Ok(jwt.GenerateToken(rnd.Next(10000).ToString()));
+        }
+
+        [HttpGet, Route("check")]
+        public IActionResult Check()
+        {
+
+            var accessToken = _httpContextAccessor.HttpContext.GetTokenAsync("access_token").Result;
+            var token = new JwtSecurityToken(accessToken);
+            string nameid = token.Claims.Where(c => c.Type == "nameid")
+                          .Select(x => x.Value).FirstOrDefault();
+
+            return Ok(nameid);
+        }
+    }
+}
---
+++ Server/Startup.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Server
+{
+    public class Startup
+    {
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            //services.AddSingleton<IConfiguration>(Configuration);
+
+            services.AddSingleton<JwtHelpers>();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddJwt(Configuration);
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
+        }
+
+        public IConfiguration Configuration { get; }
+
+    }
+    public static class Ext
+    {
+
+        public static void AddJwt(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication("Bearer")
+            .AddJwtBearer(options =>
+            {
+                // 當驗證失敗時，回應標頭會包含 WWW-Authenticate 標頭，這裡會顯示失敗的詳細錯誤原因
+                options.IncludeErrorDetails = true; // 預設值為 true，有時會特別關閉
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    // 透過這項宣告，就可以從 "sub" 取值並設定給 User.Identity.Name
+                    NameClaimType = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier",
+                    // 透過這項宣告，就可以從 "roles" 取值，並可讓 [Authorize] 判斷角色
+                    RoleClaimType = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role",
+
+                    // 一般我們都會驗證 Issuer
+                    ValidateIssuer = true,
+                    ValidIssuer = configuration.GetValue<string>("JwtSettings:Issuer"),
+
+                    // 若是單一伺服器通常不太需要驗證 Audience
+                    ValidateAudience = false,
+                    //ValidAudience = configuration["Jwt:Audience"], // 不驗證就不需要填寫
+
+                    // 一般我們都會驗證 Token 的有效期間
+                    ValidateLifetime = true,
+
+                    // 如果 Token 中包含 key 才需要驗證，一般都只有簽章而已
+                    ValidateIssuerSigningKey = false,
+
+                    // "1234567890123456" 應該從 IConfiguration 取得
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetValue<string>("JwtSettings:SignKey")))
+                };
+            });
+        }
+
+    }
+}
